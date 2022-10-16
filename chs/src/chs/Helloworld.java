4cd7ce7f9395a863c6bec3c8febba3371ea7dba2
harit <harit@LAPTOP-35LUFAN9>
2022-10-16 18:37:22 +0530

    samcode

---
+++ chs/src/chs/Helloworld.java
@@ -0,0 +1,13 @@
+package chs;
+
+public class Helloworld {
+
+	public Helloworld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
