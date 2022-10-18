9175c50a0dd4a4c6431e8f5bfa004c698b6f959b
harit <harit@LAPTOP-35LUFAN9>
2022-10-18 11:19:13 +0530

    helloworld

---
+++ helloworld/src/helloworld/MYCLASS.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class MYCLASS {
+
+	public MYCLASS() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
