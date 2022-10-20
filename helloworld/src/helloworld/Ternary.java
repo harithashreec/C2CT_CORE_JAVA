a275066edab525844272b120ddf0cff4b24d75cd
harit <harit@LAPTOP-35LUFAN9>
2022-10-20 21:52:00 +0530

    ternary

---
+++ helloworld/src/helloworld/Ternary.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class Ternary {
+
+	public Ternary() {
+		System.out.println(100);
+	}
+
+	public static void main(String[] args) {
+		int x=10;
+        x=(10==x)? 1:0;
+
+	}
+
+}
+
