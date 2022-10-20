72258a335686837d751307cb41ab3d194ae59628
harit <harit@LAPTOP-35LUFAN9>
2022-10-20 07:25:23 +0530

    typecastingdemo

---
+++ helloworld/src/helloworld/typecasting.java
@@ -0,0 +1,12 @@
+public class TypecastingDemo
+{
+    public static void(String[] args){
+        floatf=10.75f;
+        long l=(long)f;
+        double d=f;
+        byte b=12;
+        short s=b;
+        int a=l;
+        System.out.println(l);
+    }
+}
