bb741675973c727d6d2568c6eaf50e27d24495b2
harit <harit@LAPTOP-35LUFAN9>
2022-10-20 21:42:49 +0530

    if else

---
+++ helloworld/src/helloworld/hello.java
@@ -0,0 +1,17 @@
+
+package helloworld;
+
+public class hello
+{
+    public static void main (String[] args){
+        int x=10,y=11;
+        if((x<y)||(x==y))
+        {
+            System.out.println(" co is t");
+        }
+        else
+        {
+            System.out.println("f");
+        }
+    }
+}
