951d82f084a70fb9da5019571a193a6ce0989b25
harit <harit@LAPTOP-35LUFAN9>
2022-10-20 21:32:40 +0530

    if

---
+++ helloworld/src/helloworld/IFstatement.java
@@ -0,0 +1,12 @@
+public class hello
+{
+    public static void main (String[] args){
+        int x=8,y=7;
+        if(x>=y){
+            System.out.println("t");
+        }
+        {
+            System.out.println("f");
+        }
+    }
+}
