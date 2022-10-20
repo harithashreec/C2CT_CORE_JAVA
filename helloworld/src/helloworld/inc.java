6344234312a11b9a48698a3e24d3cc21327e79e7
harit <harit@LAPTOP-35LUFAN9>
2022-10-20 07:31:37 +0530

    inc

---
+++ helloworld/src/helloworld/inc.java
@@ -0,0 +1,12 @@
+
+public class hello
+{
+    public static void main (String[] args){
+        int x=10,y=20;
+        System.out.println(x++);
+         System.out.println(--x);
+          System.out.println(++y);
+    }
+}
+
+
