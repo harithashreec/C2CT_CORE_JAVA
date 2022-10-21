1f384aae46df86f3e80a861793f8d7f2cf11007b
harit <harit@LAPTOP-35LUFAN9>
2022-10-21 19:57:48 +0530

    operator

---
+++ helloworld/src/helloworld/Oparator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package helloworld;
+
+public class Oparator {
+      public static void main (String[] args)
+  
+      {
+    	  int x=5;
+    	  x+=5;
+    	  x-=5;
+    	  x*=5;
+    	  x/=5;
+    	  x%=5;
+    	  System.out.println(x);
+      }
+
+}
