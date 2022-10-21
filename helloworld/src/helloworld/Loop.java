9464fc2500a4fa918c2e91e8ecc72f90e3a3dcbb
harit <harit@LAPTOP-35LUFAN9>
2022-10-21 19:08:29 +0530

    loop

---
+++ helloworld/src/helloworld/Loop.java
@@ -0,0 +1,14 @@
+
+package helloworld;
+
+public class Loop {
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=1000;i++)
+		{
+	System.out.println("value of i:");
+				System.out.println(i);
+		
+		}
+}
+}
