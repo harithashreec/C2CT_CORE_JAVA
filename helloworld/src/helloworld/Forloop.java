61d6da7934839e68aaa728a25cc13b703ba50003
harit <harit@LAPTOP-35LUFAN9>
2022-10-21 19:05:44 +0530

    for loop

---
+++ helloworld/src/helloworld/Forloop.java
@@ -0,0 +1,14 @@
+
+package helloworld;
+
+public class Forloop {
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=1000;i++)
+		{
+	System.out.println("value of i:");
+				System.out.println(i);
+		
+		}
+}
+}
