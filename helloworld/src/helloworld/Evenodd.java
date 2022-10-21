a143ba224afb45bf62c49ed24d596757fecc71ac
harit <harit@LAPTOP-35LUFAN9>
2022-10-21 19:46:56 +0530

    even and odd

---
+++ helloworld/src/helloworld/Evenodd.java
@@ -0,0 +1,23 @@
+package helloworld;
+
+public class Evenodd {
+	
+	    public static void main(String[] args)
+	    {
+	      
+	        int num = 10;
+	 
+	        if (num % 2 == 0) {
+	 
+	            System.out.println(" Even number");
+	        }
+	 
+	        else {
+	 
+	            System.out.println( " Odd number");
+	        }
+	    }
+	}
+
+
+
