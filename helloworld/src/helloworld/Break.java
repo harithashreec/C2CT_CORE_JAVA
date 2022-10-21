357edf9739954132d70962de1aab95e081ee73d5
harit <harit@LAPTOP-35LUFAN9>
2022-10-21 20:05:03 +0530

    break

---
+++ helloworld/src/helloworld/Break.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworld;
+
+public class Break {
+	
+		public static void main(String[] args)
+		
+		{
+			for(int i=-1;i<=10;i++)
+			{
+		if(i==1)
+		{
+			continue;
+		}
+		System.out.println(i);
+	}
+
+}
+}
