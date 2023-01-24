dde3ad4b76120b910ef00183efbf0b4b2b29371f
harit <harit@host.docker.internal>
2023-01-24 17:15:43 +0530

    sqrt

---
+++ java_c/src/java/Sqrts.java
@@ -0,0 +1,20 @@
+import static java.lang.Math.*; 
+//package java;
+
+public class Sqrts {
+
+	public Sqrts() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+
+				System.out.println(sqrt(3));
+
+			}
+
+		
+
+	}
+
